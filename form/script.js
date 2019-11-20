4801608282c7c63866ca6696eafea90157b95a53
carpophorus <markomrkonjic9@gmail.com>
2019-11-20 11:33:22 +0100

    post form attempt 2

--- form/script.js
+++ form/script.js
@@ -53,8 +53,8 @@
 
     console.log(data);
     console.log(apiRoot + 'api/rgz_primedbe/post?token=testje&primedbe=' + encodeURIComponent(data));
-    //XHR.open('POST', apiRoot + 'api/rgz_primedbe/post?token=testje', true);
-    //XHR.send(data);
+    XHR.open('POST', apiRoot + 'api/rgz_primedbe/post?token=testje', true);
+    XHR.send(data);
   }
 
   document.addEventListener('DOMContentLoaded', function(event) {
